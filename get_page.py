67a4ebb6a6847c419d743203c854dda37b049e52
shuangmian-bai <2137089783@qq.com>
2025-03-18 09:37:56 +0800

    通过发请求的方式获取页码数据

--- get_page.py
+++ get_page.py
@@ -1,13 +1,15 @@
 import time
 
+import requests
 from bs4 import BeautifulSoup
 
-def get_page(driver, url, name):
-    driver.get(url + "?keyword=" + name)
-    time.sleep(1)
+def get_page(head, url, name):
+    url1 = f'https://www.cbh1.cc/public/auto/search1.html?keyword={name}'
+
+    req = requests.get(url1,headers=head)
 
     # 使用BeautifulSoup解析页面内容
-    soup = BeautifulSoup(driver.page_source, 'lxml')
+    soup = BeautifulSoup(req.text, 'lxml')
     # bs4获取最后一页
     pages = soup.select('.myci-page')[0].select('li')[-1].select('a')[0].attrs['href']
     pages = int(pages.split('=')[-1])
